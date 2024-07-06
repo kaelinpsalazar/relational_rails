4f83d34bf47ea69051401f9987bf4f0806eae3c0
Kaelin Salazar <kaelinpsalazar@gmail.com>
2024-07-06 11:37:03 -0600

    Initial commit

---
+++ db/migrate/20240706171516_create_authors.rb
@@ -0,0 +1,12 @@
+class CreateAuthors < ActiveRecord::Migration[7.1]
+  def change
+    create_table :authors do |t|
+      t.string :name
+      t.boolean :deceased
+      t.integer :age
+  
+
+      t.timestamps
+    end
+  end
+end
